152ba4d98ec18484051495e4bf024336969f3c7d
Aquilae00 <linaldi03@gmail.com>
2022-06-07 18:28:58 -0400

    analytics

---
+++ pages/_document.tsx
@@ -0,0 +1,35 @@
+import {Html, Head, Main, NextScript} from 'next/document';
+
+const GA_TRACKING_ID = 'G-ZZRE878NYY';
+export default function Document() {
+    return (
+        <Html>
+            <Head>
+                {process.env.NODE_ENV === 'production' && (
+                    <>
+                        <script
+                            async
+                            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+                        />
+                        <script
+                            dangerouslySetInnerHTML={{
+                                __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+                            }}
+                        />
+                    </>
+                )}
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
+}
